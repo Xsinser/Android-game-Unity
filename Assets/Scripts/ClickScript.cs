2263ff2adda82c74f2a0caf1d2cecaf3d5629e24
Xsinser <xsinser175@gmail.com>
2019-09-24 19:07:33 +0300

    Заливаю проект

---
+++ Assets/Scripts/ClickScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Unity;
+using System.IO;
+using System.Text;
+using System;
+using UnityEngine.UI;
+public class ClickScript : MonoBehaviour, IPointerClickHandler {    
+    private int i = 0;
+    public Material[] mater;
+    public Text txt;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        i++;
+     
+        using (StreamWriter sw = new StreamWriter(Application.persistentDataPath+"/sv.txt", false, System.Text.Encoding.Default))
+        {
+            sw.WriteLine(i);
+        }
+        Debug.Log(i);
+        TestScr a = new TestScr();
+        a.Change(i, mater, txt);
+
+        // TestScr a = new TestScr();
+        //    a.Change(i); 
+        //   this.renderer.material;
+        //  GameObject.renderer.material = GameObject.GetComponent<Script>().newMaterial;
+
+    }
+    public void Click()
+    {
+        Debug.Log("Clik");
+    }
+    // Use this for initialization
+    void Start () {
+        if (File.Exists(Application.persistentDataPath + "/sv.txt"))
+        {
+            using (StreamReader sw = new StreamReader(Application.persistentDataPath + "/sv.txt", System.Text.Encoding.Default))
+            {
+                i = Convert.ToInt16(sw.ReadLine());
+                TestScr a = new TestScr();
+                a.Change(i, mater,txt);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TestScr.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TestScr : MonoBehaviour {
+
+    GameObject objct = GameObject.Find("Cube");
+    
+    public void Change(int i, Material[] mater, Text txt)
+    {
+        switch (i)
+        {
+            case 10:
+                objct.GetComponent<Renderer>().material = mater[1];
+                break;
+            case 35:
+                objct.GetComponent<Renderer>().material = mater[2];
+                break;
+            case 95:
+                objct.GetComponent<Renderer>().material = mater[3];
+                break;
+            case 153:
+                objct.GetComponent<Renderer>().material = mater[4];
+                break;
+            case 210:
+                objct.GetComponent<Renderer>().material = mater[5];
+                break;
+            case 290:
+                objct.GetComponent<Renderer>().material = mater[6];
+                break;
+            case 350:
+                objct.GetComponent<Renderer>().material = mater[7];
+                break;
+            case 480:
+                objct.GetComponent<Renderer>().material = mater[8];
+                break;
+            case 730:
+                objct.GetComponent<Renderer>().material = mater[9];
+                break;
+            case 901:
+                objct.GetComponent<Renderer>().material = mater[10];
+                break;
+            case 1200:
+                objct.GetComponent<Renderer>().material = mater[11];
+                break;
+        }
+        txt.text = "Age: " + i.ToString();
+        if (i>=1200)
+        {
+            objct.GetComponent<Renderer>().material = mater[11];
+        }
+        if((i<1200)&&(i>900))
+        {
+            objct.GetComponent<Renderer>().material = mater[10];
+        }
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
